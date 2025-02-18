9f483862563add89d510f94b167acb33595ac384
michael-di9n <165776008+michael-di9n@users.noreply.github.com>
2025-02-18 23:59:15 +1100

    day19.py

--- day19.py
+++ day19.py
@@ -4,5 +4,5 @@ def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
     # Group by teacher_id and count the number of unique subject_ids
     result = teacher.groupby('teacher_id')['subject_id'].nunique().reset_index()
     result.rename(columns={'subject_id': 'cnt'}, inplace=True)
-    
+     
     return result
